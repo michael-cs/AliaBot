4c50eb4275375794de51e4dbc57a1ea0287f7818
michael-cs <michaelcristianodesouza@gmail.com>
2023-03-17 08:46:10 -0300

    initial commit

---
+++ cadastra_produto.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from automations import Tasks
+
+
+arquivo = "produtos.xlsx"
+data_frame = pd.read_excel(arquivo)
+url_alia = "https://painel.aliainclui.com.br/login"
+login_email = "michaelcristianodesouza@gmail.com"
+login_password = "Strike09@"
+
+
+class Cadastro():
+
+    def CadastraProdutos(data_frame):
+        Tasks.AbrePainelAlia(url_alia)
+        Tasks.EfetuaLogin(login_email, login_password)
+
+        for row in data_frame.iterrows():
+            barcode_produto = row["Código de Barras"]
+            titulo_produto = row["Título"]
+            nome_produto = row["Nome"]
+            marca_produto = row["Marca"]
+            peso_produto = row["Peso"]
+            empresa_produto = row["Empresa"]
+            descricao_produto = row["Descrição"]
+
+            Tasks.AbreCadastroDeProdutos()
+            Tasks.InsereImagem()
+            Tasks.PreencheCampos(barcode_produto,
+                                 titulo_produto,
+                                 nome_produto,
+                                 marca_produto,
+                                 peso_produto,
+                                 empresa_produto,
+                                 descricao_produto)
+        Tasks.CloseBrowser()
+
+
+Cadastro.CadastraProdutos(data_frame)
